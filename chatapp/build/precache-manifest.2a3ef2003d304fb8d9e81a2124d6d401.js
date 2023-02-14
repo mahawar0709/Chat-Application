678b9f26ee8c9e0ad1c3bbf35649e8647173f1d2
Tanu Mahawar <tanumahawar539@gmail.com>
2023-02-14 20:53:20 +0530

    first commit

---
+++ chatapp/build/precache-manifest.2a3ef2003d304fb8d9e81a2124d6d401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e66fd801f994dd598256",
+    "url": "/static/css/main.17645101.chunk.css"
+  },
+  {
+    "revision": "e66fd801f994dd598256",
+    "url": "/static/js/main.e66fd801.chunk.js"
+  },
+  {
+    "revision": "4c9aa3f9f967913d2422",
+    "url": "/static/js/1.4c9aa3f9.chunk.js"
+  },
+  {
+    "revision": "b94c00896fdfbf22fb93",
+    "url": "/static/js/2.b94c0089.chunk.js"
+  },
+  {
+    "revision": "981709b59cce671627b4",
+    "url": "/static/js/runtime~main.981709b5.js"
+  },
+  {
+    "revision": "2f17715e09b44ce666d587ed99168a73",
+    "url": "/index.html"
+  }
+];
---
+++ whatsappBackend/Server.js
@@ -0,0 +1,87 @@
+//importing
+import express from "express";
+import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import path from "path"
+// import cors from "cors"
+//app-config
+const app = express();
+const port = process.env.port || 9000;
+
+const pusher = new Pusher({
+  appId: "1553368",
+  key: "5a8696c2432a896eb9f4",
+  secret: "45176eeba0ab70f931f3",
+  cluster: "ap2",
+  useTLS: true,
+});
+
+//middleware
+app.use(express.json());
+// app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
+});
+app.use(express.static(path.join(__dirname,'./chatapp/build')))
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,"./chatapp/build/index.html"))
+})
+//DB config
+const connection_url =
+  "mongodb+srv://tanu07:GpCUYRBMGAfglQM2@cluster0.nlzx7se.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(connection_url);
+
+const db = mongoose.connection;
+
+db.once("open", () => {
+  console.log("DB connected");
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+
+  changeStream.on("change", (change) => {
+    console.log("A change occured", change);
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        Message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error triggering pusher");
+    }
+  });
+});
+
+//API routes
+app.get("/", (req, res) => res.status(200).send("Hello world"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`new message created: \n ${data}`);
+    }
+  });
+});
+
+//listener
+app.listen(port, () => {
+  console.log(`listening on local host ${port}`);
+});
